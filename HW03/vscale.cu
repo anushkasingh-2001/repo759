f5cfff9b5442892673414b4f844ad0c9a545f294
Anushka SINGH <singh434@euler-login-3.engr.wisc.edu>
2026-02-12 13:29:16 -0600

    HW03 solutions

--- HW03/vscale.cu
+++ HW03/vscale.cu
@@ -2,8 +2,8 @@
 #include "vscale.cuh"
 
 // b[i] = a[i] * b[i], each thread does at most one multiply
-__global__ void vscale(const float* a, float* b, int n) {
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void vscale(const float* a, float* b, unsigned int n) {
+  unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
   //thread index must be less than the size of the elements (n)
   if (i < n) {
